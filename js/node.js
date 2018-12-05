36542154f3a052ed779abc2ddb0fac1b20a9dea8
Jevaeson <zunyue1995@JevaesondeMacBook-Pro.local>
2018-12-05 16:05:49 +0800

    new

---
+++ js/node.js
@@ -0,0 +1,14 @@
+var b = require('./button.js')
+// 导入自定义模块的方法
+// require('路径')
+// 导入第三方模块和核心模块
+// require('模块名')
+const $ = require('jquery')
+
+$.each([1, 2, 3], function(ele) {
+  console.log(ele)
+})
+
+console.log(b)
+const a = 10
+console.log(a)
